ca8e9b1be07cfd570b4bc33e965194230f09a4c7
nscetintas <107845038+nscetintas@users.noreply.github.com>
2026-09-05 15:26:41 +0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXX",
+  apiKey: "AIzaSyBVqIpdvpJHjF_mrIvo8vsba6VhEgLY664",
   authDomain: "irs-student-development.firebaseapp.com",
   projectId: "irs-student-development",
   storageBucket: "irs-student-development.firebasestorage.app",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  messagingSenderId: "843517742734",
+  appId: "1:843517742734:web:f324ce5f8025a900f343df",
+  measurementId: "G-CZB5NY5PX8"
 };
 
 // UI-level allowlist. Firestore Security Rules are the real security boundary.
